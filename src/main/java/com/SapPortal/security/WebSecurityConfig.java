784476fc0ee0857d4159686312552d7794962c48
vinavp1 <piyushpagar79@gmail.com>
2022-09-04 16:21:00 +0530

    adding swagger for easy understanding for frontend devloper

--- src/main/java/com/SapPortal/security/WebSecurityConfig.java
+++ src/main/java/com/SapPortal/security/WebSecurityConfig.java
@@ -55,8 +55,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-				.antMatchers("/api/auth/**").permitAll().antMatchers("/api/test/**").permitAll().anyRequest()
-				.authenticated();
+				.antMatchers("//api/auth/**").permitAll().antMatchers("/api/test/**").permitAll()
+				.anyRequest().authenticated();
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 }
+//.antMatchers("/**").permitAll()  add this before 58 line start
