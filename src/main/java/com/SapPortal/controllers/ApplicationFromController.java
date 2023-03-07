3a41c9918a6fa328e204b955bb412fc600dbcbf0
Sachin Jagtap <100439480+Sachin3302@users.noreply.github.com>
2023-03-07 13:20:53 +0530

    crossorigin

--- src/main/java/com/SapPortal/controllers/ApplicationFromController.java
+++ src/main/java/com/SapPortal/controllers/ApplicationFromController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ import com.SapPortal.models.ApplicationForm;
 import com.SapPortal.repository.ApplicationFormRepository;
 import com.SapPortal.security.services.ApplicationFromService;
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class ApplicationFromController {
